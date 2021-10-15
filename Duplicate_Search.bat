a1d5edcf6db74a5917eb063ff1e18290b3bed17a
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2021-10-15 14:26:31 +0000

    Add files via upload

--- Duplicate_Search.bat
+++ Duplicate_Search.bat
@@ -13,59 +13,64 @@
 ::     IB_U_Z_Z_A_R_Dl
 ::
 :: CREDITS
+::     @Grub4K - Helped reducing variables plural algorithm.
 ::     @Grub4K - Teached me the tuple pseudo-data pattern.
 ::     @Grub4K - Creator of the timer algorithm.
+::     @Sintrode - Helped me encoding the CLI.
 ::     A project created in the "server.bat" Discord: https://discord.gg/GSVrHag
 ::------------------------------------------------------------------------------
+cls
+>nul chcp 65001
+setlocal DisableDelayedExpansion
 cd /d "%~dp0"
-set "title=title [!percentage!/100%%]  ^|  [!result! duplicate!s_result! found from !index_1! indexed file!s_index!] - Duplicate Search"
-Setlocal EnableDelayedExpansion
-for /f "tokens=1-4delims=:.," %%a in ("!time: =0!") do set /a "t1=(((1%%a*60)+1%%b)*60+1%%c)*100+1%%d-36610100"
-set /a percentage=0, result=0, index_1=0, index_2=1
-for /f "tokens=2delims=:." %%a in ('chcp') do set /a "CP=%%a"
-for /f "tokens=4,5delims=. " %%a in ('ver') do (
-    if "%%a.%%b"=="10.0" (
-        set cyan=[36m
-        set yellow=[33m
-        set scan=comp "%%x" "%%y" /m
+set "@TITLE=title [!Percentage!/100%%]  ^|  [!Results! duplicate!s_Results! found from !Index_1! indexed file!s_Index_1!] - Duplicate Search"
+set "@SET_S=if !?! gtr 1 (set s_?=s) else (set s_?=)"
+setlocal EnableDelayedExpansion
+for /f "tokens=4,5delims=. " %%A in ('ver') do (
+    if "%%A.%%B"=="10.0" (
+        set CYAN=[36m
+        set YELLOW=[33m
+        set @SCAN=comp "%%C" "%%D" /m
     ) else (
-        set scan=fc "%%x" "%%y"
+        set @SCAN=fc "%%C" "%%D"
     )
 )
+for %%A in (s_Index s_Result s_Second) do set %%A=
+set /a Results=0, Index_1=0, Index_2=1
 echo.
-echo !cyan!Searching for duplicate(s) in "%~dp0" ...
-echo.
-echo �������������������������
->nul chcp 65001
-for /f "delims=" %%a in ('dir /a:-d /b /o:-s /s') do (
-    set /a index_1+=1
-    set "cache[!index_1!]=%%a"
-    if !index_1! gtr 1 set s_index=s
-    %title%
+echo  !CYAN!■ Searching for duplicates in "%~dp0" ...
+echo  ├──────────────────────────────────────────────────────────────────────────────
+for /f "tokens=1-4delims=:.," %%A in ("!time: =0!") do set /a "t1=(((1%%A*60)+1%%B)*60+1%%C)*100+1%%D-36610100"
+for /f "delims=" %%A in ('dir /a:-d /b /o:-s /s') do (
+    set /a Index_1+=1
+    set "Cache[!Index_1!]=%%A"
+    %@SET_S:?=Index_1%
+    %@TITLE%
 )
-for /l %%a in (1 1 !index_1!) do (
-    set /a index_2+=1, percentage=%%a*100/index_1
-    for /l %%b in (!index_2! 1 !index_1!) do (
-        for /f "tokens=1,2delims=`" %%x in ("!cache[%%a]!`!cache[%%b]!") do (
-            if "%%~zx"=="%%~zy" (
-                >nul 2>&1 %scan% && (
-                    set /a result+=1
-                    if !result! gtr 1 set s_result=s
-                    echo The !yellow!"%%x"!cyan! and !yellow!"%%y"!cyan! files are duplicated.
+for /l %%A in (1 1 !Index_1!) do (
+    set /a Percentage=%%A*100/Index_1, Index_2+=1
+    for /l %%B in (!Index_2! 1 !Index_1!) do (
+        for /f "tokens=1,2delims=`" %%C in ("!Cache[%%A]!`!Cache[%%B]!") do (
+            if "%%~zC"=="%%~zD" (
+                >nul 2>&1 %@SCAN% && (
+                    set /a Results+=1
+                    %@SET_S:?=Results%
+                    echo  ├ The !YELLOW!"%%C"!CYAN! and !YELLOW!"%%D"!CYAN! files are duplicated.
                 )
             )
         )
     )
-    %title%
+    %@TITLE%
 )
->nul chcp !CP!
-echo �������������������������
-for /f "tokens=1-4delims=:.," %%a in ("!time: =0!") do set /a "t2=(((1%%a*60)+1%%b)*60+1%%c)*100+1%%d-36610100, tDiff=t2-t1, tDiff+=((~(tDiff&(1<<31))>>31)+1)*8640000, seconds=tDiff/100"
-if !seconds! gtr 1 set s_s=s
-%title%
+if !Results!==0 echo  │ No duplicate file found.
+for /f "tokens=1-4delims=:.," %%A in ("!time: =0!") do set /a "t2=(((1%%A*60)+1%%B)*60+1%%C)*100+1%%D-36610100, tDiff=t2-t1, tDiff+=((~(tDiff&(1<<31))>>31)+1)*8640000, Seconds=tDiff/100"
+echo  └──────────────────────────────────────────────────────────────────────────────
+set Percentage=100
+%@TITLE%
+%@SET_S:?=Seconds%
 echo.
-echo Scan complited with !result! duplicate!s_result! found from !index_1! indexed file!s_index! in !seconds! second!s_s!.
+echo  ■ Scan complited with !Results! duplicate!s_Results! found from !Index_1! indexed file!s_Index_1! in !Seconds! second!s_Seconds!.
 echo.
-<nul set /p="Press {ANY KEY} to exit..."
+<nul set /p= ■ Press {ANY KEY} to exit...
 >nul pause
 exit
